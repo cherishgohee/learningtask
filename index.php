486abb93b2ba4c60c1e6a7b6290d7c5c03c1e0f3
cherishgohee <100120673+cherishgohee@users.noreply.github.com>
2022-04-04 23:40:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Cherish Gohee! </h1>
+  <ul>
+    <li><a href="index.php" target="_blank"> HOME </a></li>
+    <li><a href="/LT2/Homepage.html" target= "_blank"> LEARNING TASK 2 </a> </li>
+    <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+    <li><a href="/CLE1MT/index.html" target= "_blank"> CLE MIDTERM</a> </li>
+    <li><a href="/LT4/index.html" target= "_blank">LEARNING TASK 4</a> </li>
+    </ul>
+  </body>
+</html>
