28ffddb8cb0629e6640f985a7aa7b9211eeb47f2
cherishgohee <100120673+cherishgohee@users.noreply.github.com>
2022-05-22 02:15:10 +0800

    Add files via upload

---
+++ files/cpg.js
@@ -0,0 +1,34 @@
+function open1() {
+    document.getElementById("prev1").style.display = "block";
+}
+function close1(){
+    document.getElementById("prev1").style.display = "none";
+}
+
+function open2() {
+    document.getElementById("prev2").style.display = "block";
+}
+function close2(){
+    document.getElementById("prev2").style.display = "none";
+}
+
+function open3() {
+    document.getElementById("prev3").style.display = "block";
+}
+function close3(){
+    document.getElementById("prev3").style.display = "none";
+}
+
+function open4() {
+    document.getElementById("prev4").style.display = "block";
+}
+function close4(){
+    document.getElementById("prev4").style.display = "none";
+}
+
+function open5() {
+    document.getElementById("prev5").style.display = "block";
+}
+function close5(){
+    document.getElementById("prev5").style.display = "none";
+}
